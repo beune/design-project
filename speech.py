99b13f200776b4106afd381b530a57f2e874b3c9
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-02-08 10:51:00 +0100

    added speech.py as test

---
+++ speech.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Say anything')
+    audio = r.listen(source)
+
+    text = r.recognize_google(audio)
+    print('You said : {}'.format(text))
