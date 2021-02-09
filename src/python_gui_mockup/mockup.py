05ecaaa4506b75cd3836baeea382a4fdefa4fc66
Pim Beune <git@beune.dev>
2021-02-09 12:33:22 +0100

    Improve UX, add radio buttons for different views
    Alignment of the items used to be very strange when resizing the window.
    Radio buttons are added to switch between the different views:
     - File View
     - Tree View

--- src/python_gui_mockup/mockup.py
+++ src/python_gui_mockup/mockup.py
@@ -7,51 +7,71 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 
 root = tk.Tk()
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-row = 1
+radioVar = tk.IntVar()
+radioVar.set(0)
+
+def radio():
+    choice  = radioVar.get()
+    if choice == 0:
+       tree_view_frame.grid()
+       file_view_frame.grid_remove()
+
+    elif choice == 1:
+       tree_view_frame.grid_remove()
+       file_view_frame.grid()
+    else:
+        # TODO
+        pass
 
 def quit():
     if messagebox.askyesno("Afsluiten", "Wilt u echt het programma afsluiten?"):
         root.destroy()
 
-def toggle():
-    global row
-    ttk.Entry(values_frame).grid(row=row, column=0, sticky="NEW", pady=2, padx=2)
-    ttk.Entry(values_frame).grid(row=row, column=1, sticky="NEW", pady=2, padx=2)
-    row += 1
-
-content = ttk.Frame(root, padding=(3,3,3,3))
+content = ttk.Frame(root, padding=(10,10,10,10))
 text_frame = ttk.Frame(content, borderwidth=5, relief="ridge")
+results_frame = ttk.Frame(content, borderwidth=5, relief="ridge")
 # TODO text starts vertically in the middle and does not wrap
-text_entry = ttk.Entry(text_frame)
-values_frame = ttk.Frame(content, borderwidth=5)
+text_entry = ttk.Entry(text_frame).grid(column=0, row=0, sticky="NSEW")
+start_stop_button = ttk.Button(content, text="Start/stop").grid(column=0, row=1, sticky="NSEW")
 
-start_stop_button = ttk.Button(content, text="Start/stop", command=toggle)
+results_frame.grid(column=1, row=0, rowspan=2, sticky=("NSEW"))
+results_frame.columnconfigure(0, weight=1)
+results_frame.columnconfigure(1, weight=1)
+results_frame.rowconfigure(1, weight=1)
 
 content.grid(column=0, row=0, sticky=("NSEW"))
-text_frame.grid(column=0, row=0, columnspan=3, rowspan=4, sticky=("NSEW"))
-text_entry.grid(column=0, row=0, sticky="NSEW")
-values_frame.grid(column=3, row=0, columnspan=1, sticky=("NSEW"))
-start_stop_button.grid(column=3, row=3, sticky=("EW"))
+content.columnconfigure(0, weight=1)
+content.columnconfigure(1, weight=1)
+content.rowconfigure(0, weight=1)
 
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
+# To make the text entry full-size
+text_frame.grid(column=0, row=0, sticky=("NSEW"))
 text_frame.columnconfigure(0, weight=1)
 text_frame.rowconfigure(0, weight=1)
 
-values_frame.columnconfigure(0, weight=1)
-values_frame.columnconfigure(1, weight=1)
+tk.Radiobutton(results_frame, text="Tree View", variable=radioVar, value=0, command=radio).grid(column=0, row=0)
+tk.Radiobutton(results_frame, text="File View", variable=radioVar, value=1, command=radio).grid(column=1, row=0)
 
-content.columnconfigure(0, weight=3)
-content.columnconfigure(1, weight=3)
-content.columnconfigure(2, weight=3)
-content.columnconfigure(3, weight=3)
-content.rowconfigure(1, weight=1)
+tree_view_frame = ttk.Frame(results_frame)
+tree_view_frame.grid(column=0, row=1, columnspan=2, sticky="NSEW")
+tree_view_frame.columnconfigure(0, weight=1)
+tree_view_frame.rowconfigure(0, weight=1)
+tree_view_label = ttk.Label(tree_view_frame, text="Tree").grid(column=0, row=0, sticky="NSEW")
+
+file_view_frame = ttk.Frame(results_frame)
+file_view_frame.grid(column=0, row=1, columnspan=2, sticky="NSEW")
+file_view_frame.columnconfigure(0, weight=1)
+file_view_frame.rowconfigure(0, weight=1)
+file_view_label = ttk.Label(file_view_frame, text="File").grid(column=0, row=0, sticky="NSEW")
+
 
 if __name__ == "__main__":
-    # root.protocol('WM_DELETE_WINDOW', quit)
+    root.protocol('WM_DELETE_WINDOW', quit)
     try:
+        radio()
         root.mainloop()
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, tk.TclError):
         exit()
