74f54a76a8c8807c4c954c18cde5deb1e5f7dfd5
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-02-10 13:30:17 +0100

    small extra

--- utilities_test.py
+++ utilities_test.py
@@ -5,10 +5,12 @@ from utilities import *
 
 class MyTestCase(unittest.TestCase):
     def test_combine(self):
+        # combine two overlapping strings
         first = "dit is een"
         second = "een korte boodschap"
         self.assertEqual("dit is een korte boodschap", combine(first, second))
 
+        # append the strings if no overlap
         first = "dit is een"
         second = "korte boodschap"
         self.assertEqual("dit is een korte boodschap", combine(first, second))
@@ -17,6 +19,7 @@ class MyTestCase(unittest.TestCase):
         second = "fghijk"
         self.assertEqual("abcdefghijk", combine(first, second, ""))
 
+        # don't fail if something is empty
         first = ""
         second = "abc"
         self.assertEqual("abc", combine(first, second, ""))
@@ -28,6 +31,11 @@ class MyTestCase(unittest.TestCase):
         first = ""
         second = ""
         self.assertEqual("check", combine(first, second, "check"))
+
+        # combine the largest amount of characters
+        first = "ho ho ho"
+        second = "ho ho merry crisis"
+        self.assertEqual("ho ho ho merry crisis", combine(first, second))
 
 
 
