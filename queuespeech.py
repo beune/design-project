51c35522dd456cd40501a74620ecd3399d6fb430
Stan Ritsema <s.ritsema@student.utwente.nl>
2021-02-10 15:08:40 +0100

    Removed old recognize method

--- queuespeech.py
+++ queuespeech.py
@@ -24,7 +24,6 @@ class QueueSpeech:
     def __init__(self):
         self.frames = []
         self.text = ""
-        self.processed = 0
 
     def listen_and_chop(self):
         stream = p.open(format=FORMAT,
@@ -38,59 +37,10 @@ class QueueSpeech:
                 data = stream.read(CHUNK)
                 self.frames.append(data)
 
-    def recognize_worker(self):
-        # this runs in a background thread
-        while True:
-            length = len(self.frames)
-            if self.processed < length - 20:
-                newframes = []
-                i = self.processed
-                while i < length - 20:
-                    newframes.append(self.frames[i])
-                    i += 1
-                data = b"".join(newframes)
-                audio = sr.AudioData(data, RATE, p.get_sample_size(FORMAT))
-                try:
-                    self.text = self.text + " " + r.recognize_google(audio, language="nl-NL")
-                    self.processed = i
-                    window_frames = []
-                    for j in range(self.processed, length - 1):
-                        window_frames.append(self.frames[j])
-                    window_data = b"".join(window_frames)
-                    window_audio = sr.AudioData(window_data, RATE, p.get_sample_size(FORMAT))
-                    try:
-                        print(self.text + r.recognize_google(window_audio, language="nl-NL"))
-                    except sr.UnknownValueError:
-                        print(self.text)
-                except sr.UnknownValueError:
-                    pass
-            else:
-                frame_data = b"".join(self.frames)
-                audio = sr.AudioData(frame_data, RATE, p.get_sample_size(FORMAT))
-                if audio is None:
-                    print("no audio")  # stop processing if the main thread is done
-                else:
-
-                    # received audio data, now we'll recognize it using Google Speech Recognition
-                    try:
-                        # for testing purposes, we're just using the default API key
-                        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-                        # instead of `r.recognize_google(audio)`
-                        print(self.text + r.recognize_google(audio, language="nl-NL"))
-                    except sr.UnknownValueError:
-                        # print("Google Speech Recognition could not understand audio")
-                        pass
-                    except sr.RequestError as e:
-                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-            time.sleep(0.5)
-
     def recognize(self):
-        lower = 0
         last = 0
-        upper = 0
         window = 80
         delta = 20
-        overlap = window
         uncertain = window
         assert delta <= uncertain
         while True:
@@ -101,7 +51,6 @@ class QueueSpeech:
             min_frame = max(0, last - delta)
             max_frame = min(upper + delta, length)
             frames = self.frames[min_frame:max_frame]
-
             data = b"".join(frames)
             audio = sr.AudioData(data, RATE, p.get_sample_size(FORMAT))
             try:
@@ -114,7 +63,7 @@ class QueueSpeech:
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
             last = upper  # (upper)
-            time.sleep(2)
+            # time.sleep(0.25)
 
     def startup(self):
         # start a new thread to recognize audio, while this thread focuses on listening
@@ -122,7 +71,6 @@ class QueueSpeech:
         listen_thread.start()
         recognize_thread = Thread(target=self.recognize, daemon=True)
         recognize_thread.start()
-        recognize_thread.join()  # doet nu niets
 
 
 if __name__ == "__main__":
