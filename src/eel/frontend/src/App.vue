4c41238e0cd1877ffd3a2aafc6aafd23c421439b
Pim Beune <git@beune.dev>
2021-03-08 11:11:21 +0100

    Initial commit

---
+++ src/eel/frontend/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-primary">
+      <div class="md-toolbar-section-start">
+        <span class="md-title">Mammografie 22-09-16</span>
+      </div>
+      <div class="md-toolbar-section-end">
+        <md-menu md-align-trigger md-size="auto">
+          <md-button md-menu-trigger>Mammo</md-button>
+          <md-menu-content>
+          </md-menu-content>
+        </md-menu>
+      </div>
+      </md-app-toolbar>
+
+      <md-app-drawer md-permanent="full">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          Navigatie
+        </md-toolbar>
+
+        <md-list>
+          <!-- NB: md-offset gives prop warning but this has to be fixed in the upstream, I have filed an issue on GitHub https://github.com/vuematerial/vue-material/issues/2315 -->
+          <md-list-item>
+            <md-menu md-align-trigger md-size="auto" md-offset-x="127" :md-offset-y="-36">
+              <md-button md-menu-trigger><md-icon>save</md-icon> Bestand</md-button>
+              <md-menu-content>
+                <md-menu-item>Nieuw</md-menu-item>
+                <md-menu-item>Openen...</md-menu-item>
+                <md-menu-item>Opslaan</md-menu-item>
+                <md-menu-item>Opslaan als...</md-menu-item>
+                <md-menu-item>Exporteren</md-menu-item>
+                <md-menu-item>Voorkeuren...</md-menu-item>
+                <md-menu-item>Afsluiten</md-menu-item>
+              </md-menu-content>
+            </md-menu>
+          </md-list-item>
+
+          <md-list-item>
+            <md-menu md-align-trigger md-size="auto" md-offset-x="127" :md-offset-y="-36">
+              <md-button md-menu-trigger><md-icon>fit_screen</md-icon> Beeld</md-button>
+            </md-menu>
+          </md-list-item>
+
+          <md-list-item>
+            <md-menu md-align-trigger md-size="auto" md-offset-x="127" :md-offset-y="-36">
+              <md-button md-menu-trigger><md-icon>help</md-icon> Help</md-button>
+            </md-menu>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <d3orgtree/>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<style lang="scss">
+  .md-app {
+    height: 100vh;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+</style>
+
+<script>
+import d3orgtree from "./components/d3-org-tree.vue"
+export default {
+  components: {
+    d3orgtree,
+  },
+  name: 'Normal',
+  data: () => ({
+  }),
+  methods: {
+    test_function () {
+      window.eel.test("test");
+    }
+  },
+}
+</script>
---
+++ src/eel/frontend/src/components/d3-org-tree.vue
@@ -0,0 +1,107 @@
+<template>
+        <!--TODO make tree view fit to entire width and height-->
+        <div class="svgContainer"></div>
+</template>
+<style lang="scss">
+    .domStyle {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+
+        * {
+            display: inline-block;
+            font-weight: bold;
+            font-size: 40px;
+        }
+    }
+</style>
+<script>
+import data from './data.json'
+    import OrgTree from "d3-org-tree";
+    export default {
+        name: "Chart",
+        data: () => ({
+            //TODO FIX ORIENTATION
+            chartReference: null,
+            straights: [{text: "straight", value: true}, {text: "curve", value: false}],
+            orientation: {value: 'left-to-right'},
+            straight: {text: "curve", value: false},
+            displayArrow: true,
+            straightLink: false,
+        }),
+        watch: {
+            data(value) {
+                this.renderChart(value);
+            }
+        },
+        mounted() {
+            this.renderChart(data)
+        },
+        methods: {
+            renderChart(data) {
+                if (!this.chartReference) {
+                    this.chartReference = new OrgTree();
+                }
+                this.chartReference
+                    .container('.svgContainer')
+                    //.svgWidth(800)
+                    //.svgHeight(600)
+                    .data(data)
+                    //.marginLeft(-50)
+                    .highlight({
+                        "borderWidth": 1,
+                        "borderRadius": 15,
+                        "borderColor": {
+                            "red": 50,
+                            "green": 255,
+                            "blue": 30,
+                            "alpha": 1
+                        },
+                        "backgroundColor": {
+                            "red": 20,
+                            "green": 100,
+                            "blue": 40,
+                            "alpha": 1
+                        }
+                    })
+                    //.current('O-2')
+                    .displayArrow(this.displayArrow)
+                    .straightLink(this.straightLink)
+                    .collapsible(false)
+                    .onNodeClick(d => {
+                        console.log(d + " node clicked")
+                    })
+                    .onNodeAdd(d => {
+                        console.log(d + " node added")
+                    })
+                    .onNodeRemove(d => {
+                        console.log(d + " node removed")
+                        this.chartReference.removeNode(d)
+                    })
+                    .render();
+
+
+                /*
+                //mock trigger click
+                setTimeout(() => {
+                    d3.select('#O-3').dispatch('click')
+                }, 2000)*/
+
+            },
+
+            transformLayout(direction) {
+                this.chartReference.transformLayout(direction)
+            },
+
+            transformStraightLink(straight) {
+                this.chartReference.transformStraightLink(straight)
+            },
+
+            toggleArrow(displayArrow) {
+                this.chartReference.toggleArrow(displayArrow)
+            }
+        }
+    }
+
+</script>
