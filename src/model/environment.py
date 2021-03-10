489b55ffa59a1531137f57823bae5955c85d8907
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-03-10 10:23:49 +0100

    Removed structure

--- src/model/environment.py
+++ src/model/environment.py
@@ -1,7 +1,6 @@
 """
 Imports
 """
-from structure import Structure
 from hinter import Hinter
 from nlp import NLP
 
@@ -11,34 +10,8 @@ class Environment(object):
     Environment class which is interchangeable for different types of radiology
     Each environment contains:
         - Natural Language Processer, used to process the natural language into labelled XML
-        - Structure, used to generate a user-friendly structure out of the XML
         - Hinter, used to provide hints based on the current XML
     """
-    def __init__(self, nlp: NLP, structure: Structure, hinter: Hinter):
+    def __init__(self, nlp: NLP, hinter: Hinter):
         self.nlp = nlp
-        self.structure = structure
         self.hinter = hinter
-
-    @property
-    def nlp(self) -> NLP:
-        """
-        Method used to retrieve the current Natural Language Processing algorithm that is used
-        :return: The current NLP algorithm
-        """
-        return self.nlp
-
-    @property
-    def structure(self) -> Structure:
-        """
-        Method used to retrieve the current structure that is used
-        :return: The current structuring algorithm
-        """
-        return self.structure
-
-    @property
-    def hinter(self) -> Hinter:
-        """
-        Method used to retrieve the current hinter that is used
-        :return: The current hinting algorithm
-        """
-        return self.hinter
