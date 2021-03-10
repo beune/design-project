089a7cd2e39a3b3413463186b67b7fa63c4901a6
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-03-10 11:15:26 +0100

    Removed private properties from ReportNode/ReportLabel

--- src/model/report_leaf.py
+++ src/model/report_leaf.py
@@ -8,48 +8,16 @@ class ReportLeaf:
     Class used to represent Nodes
     """
     def __init__(self, text: str, label: str, certainty: float, hint: str = None):
-        self._text = text
-        self._label = label
-        self._certainty = certainty
-        self._hint = hint
+        self.text = text
+        self.label = label
+        self.certainty = certainty
+        self.hint = hint
 
     def __repr__(self):
-        return 'Leaf("{}", {}, {})'.format(self._text, self._label, self._certainty)
+        return 'Leaf("{}", {}, {})'.format(self.text, self.label, self.certainty)
 
     def __eq__(self, other):
         return type(self) == type(other) \
                and self.text == other.text \
                and self.label == other.label \
                and self.certainty == other.certainty
-
-    @property
-    def text(self):
-        """
-
-        :return: Returns the text of the report leaf
-        """
-        return self._text
-
-    @property
-    def label(self):
-        """
-
-        :return: Returns the label of the report leaf
-        """
-        return self._label
-
-    @property
-    def certainty(self):
-        """
-
-        :return: Returns the certainty of the labelling of the leaf
-        """
-        return self._certainty
-
-    @property
-    def hint(self):
-        """
-
-        :return: Returns the hint of the leaf
-        """
-        return self._hint
--- src/model/report_node.py
+++ src/model/report_node.py
@@ -8,15 +8,15 @@ class ReportNode:
     Class used to represent Nodes
     """
     def __init__(self, label: str, children: list = None, expects: list = None):
-        self._label = label
-        self._children = [] if children is None else children
-        self._expects = expects
+        self.label = label
+        self.children = [] if children is None else children
+        self.expects = expects
 
     def __repr__(self):
-        return 'Node({})'.format(self._label)
+        return 'Node({})'.format(self.label)
 
     def __iter__(self):
-        for child in self._children:
+        for child in self.children:
             yield child
 
     def __eq__(self, other):
@@ -24,33 +24,9 @@ class ReportNode:
                and self.label == other.label \
                and self.children == other.children
 
-    @property
-    def label(self) -> str:
-        """
-
-        :return:
-        """
-        return self._label
-
-    @property
-    def children(self) -> list:
-        """
-
-        :return:
-        """
-        return self._children
-
-    @property
-    def expects(self) -> list:
-        """
-
-        :return:
-        """
-        return self._expects
-
     def add_child(self, child):
         """
         Method used to update the children of a ReportNode
         :param child: The new child that needs to be added to the children list
         """
-        self._children.append(child)
+        self.children.append(child)
