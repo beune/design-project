eeb758d9e2da4e1f23e2b45ef992ff1f938c3608
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-03-11 21:44:55 +0100

    added alternatives to report_leaf.py

--- test/test_report_leaf.py
+++ test/test_report_leaf.py
@@ -8,19 +8,19 @@ from src.report_leaf import ReportLeaf
 
 class ReportLeafTest(unittest.TestCase):
     def test_init(self):
-        text = "text"
+        text = "value"
         label = "label"
         certainty = 0.85
         hint = "hint"
-        leaf = ReportLeaf(text, label, certainty, hint)
-        self.assertEqual(leaf.text, text)
+        leaf = ReportLeaf(label, {text: certainty}, hint)
         self.assertEqual(leaf.label, label)
-        self.assertEqual(leaf.certainty, 0.85)
+        self.assertEqual(leaf.values, {text: certainty})
         self.assertEqual(leaf.hint, hint)
 
     def test_eq(self):
-        leaf1 = ReportLeaf("text1", "label1", 0.5, "hint1")
-        leaf2 = ReportLeaf("text2", "label2", 0.6, "hint2")
-        self.assertTrue(leaf1 == leaf1)
+        leaf1 = ReportLeaf("text1", {"label1": 0.5}, "hint1")
+        leaf1a = ReportLeaf("text1", {"label1": 0.5}, "hint1")
+        leaf2 = ReportLeaf("text2", {"label2": 0.6}, "hint2")
+        self.assertTrue(leaf1 == leaf1a)
         self.assertTrue(leaf2 == leaf2)
         self.assertFalse(leaf1 == leaf2)
