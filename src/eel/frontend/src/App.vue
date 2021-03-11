68ea812b3f4fd2599913e52d5922495ac299a2d4
TomH 123 <t.h.postmus@student.utwente.nl>
2021-03-11 22:14:05 +0100

    Added top menu bar, and options for the File (Bestand) menu

--- src/eel/frontend/src/App.vue
+++ src/eel/frontend/src/App.vue
@@ -1,14 +1,72 @@
 <template>
   <v-app>
-    <v-app-bar :elevation="0" dense>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-          Bestand Beeld Help
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-app-bar>
+      <v-app-bar id="top-menu-bar" :elevation="0" height="29px" dense color="primary">
+        <v-menu
+          bottom
+          :offset-y="true"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="text-none"
+              tile
+              text
+              elevation="0"
+              color="white"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Bestand
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item link>
+              <v-list-item-title>Nieuw</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-title>Openen...</v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item link>
+              <v-list-item-title>Opslaan</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-title>Opslaan als...</v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item link>
+              <v-list-item-title>Exporteren naar schijf...</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-title>Exporteren naar database...</v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item link>
+              <v-list-item-title>Voorkeuren...</v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+        </v-menu>
+        <v-btn class="text-none" tile text elevation="0" color="white">
+          Beeld
+        </v-btn>
+        <v-btn class="text-none" tile text elevation="0" color="white">
+          Help
+        </v-btn>
+      </v-app-bar>
+
     <!-- Sizes your content based upon application components -->
     <v-main>
     <v-app-bar :elevation="0" dense>
@@ -31,11 +89,18 @@
 </template>
 
 <style lang="scss">
-/*TODO remove browser scrollbar*/
+#menu-bar-wrapper {
+  border-bottom: black 1px;
+}
+
+.body {
+  font-family: "Segoe UI", serif;
+}
 </style>
 
 <script>
 import d3orgtree from "./components/d3-org-tree.vue"
+
 export default {
   components: {
     d3orgtree,
