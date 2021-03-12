c5ec2e8b4bd82dd028011abadf550c70be4030a3
bas_k <bas_kool@live.nl>
2021-03-12 14:57:13 +0100

    Added certainty, removed classes for idcounter and nodes and created a nested function for them.

--- src/client/view.py
+++ src/client/view.py
@@ -9,36 +9,6 @@ import json
 import pprint
 
 
-class IdCounter:
-    """
-    Class used to keep track of the node ids while creating the nodes list
-    """
-
-    def __init__(self):
-        self.identifier = 0
-
-    def increase(self):
-        """
-        Increases the identifier, called when a new node is created
-        """
-        self.identifier += 1
-
-
-class Nodes:
-    """"
-    Class used to store the node list that's being created
-    """
-
-    def __init__(self):
-        self.nodes = []
-
-    def add_node(self, node: dict):
-        """
-        Adds a node and all it's information to the list of nodes
-        """
-        self.nodes.append(node)
-
-
 def get_tree(text: str):
     """
     Sends the input text to and the gets the tree from the controller.
@@ -50,73 +20,92 @@ def get_tree(text: str):
     return tree
 
 
-def traverse(root: ReportNode, id_count: IdCounter, nodes: Nodes, parent_id: int = None):
+def get_tree2(tree: ReportNode):
+    """"
+    Function that traverses through the given ReportNode, and uses the make_node function to create nodes and leaves
+    in the right format for the UI.
+    :param tree: The reportnode created by the NLP, containing the entire structure
+    :return: Structured data for the front end in Json
     """
-    Function which traverses through tree by calling recursively calling its children,
-    while calling the add_nodes function on each node.
-    :param root: The node currently being traversed, containing all necessary information including children
-    :param id_count: The class containing the id of the previously traversed node
-    :param nodes: Class containing the list of nodes, needed for the add_node function
-    :param parent_id: The id of the parent of the currently traversed node, needed in the add_node function
-    """
-    identifier = id_count.identifier
-    add_node(identifier, parent_id, root.label, nodes)
-    id_count.increase()
-    for child in root:
-        if isinstance(child, ReportLeaf):
-            process_leaf(child, identifier, id_count, nodes)
-        else:
-            traverse(child, id_count, nodes, identifier)
+    nodes = []
+    new_id = 0
+
+    def traverse(root: ReportNode, parent_id: int = None):
+        """
+        Function which traverses through tree by calling recursively calling its children,
+        while calling the add_nodes function on each node.
+        :param root: The node currently being traversed, containing all necessary information including children
+        :param parent_id: The id of the parent of the currently traversed node, needed in the add_node function
+        """
+        nonlocal new_id
+        nodes.append(make_node(new_id, parent_id, root.label))
+        parent = new_id
+        new_id += 1
+        for child in root:
+            if isinstance(child, ReportLeaf):
+                process_leaf(child, parent)
+            else:
+                traverse(child, parent)
+
+    def process_leaf(leaf: ReportLeaf, parent_id: int):
+        """
+        Calls the add_nodes function on the leaves and their text, adding 2 nodes
+        :param leaf: The leaf currently being traversed, containing all necessary information including its text
+        :param parent_id: The id of the parent of the currently traversed node, needed in the add_nodes function
+        """
+        nonlocal new_id
+        nodes.append(make_node(new_id, parent_id, leaf.label, leaf.certainty))
+        old_id = new_id
+        new_id += 1
+
+        nodes.append(make_node(new_id, old_id, leaf.label))
+        new_id += 1
+
+    traverse(tree)
+    return nodes
 
 
-def process_leaf(leaf: ReportLeaf, parent_id: int, id_count: IdCounter, nodes: Nodes):
-    """
-    Calls the add_nodes function on the leaves and their text, adding 2 nodes
-    :param leaf: The leaf currently being traversed, containing all necessary information including its text
-    :param parent_id: The id of the parent of the currently traversed node, needed in the add_nodes function
-    :param id_count: The class containing the id of the previously traversed node, used to set new id's
-    :param nodes: Class containing the list of nodes, needed for the add_node function
-    """
-    identifier_node = id_count.identifier
-    add_node(identifier_node, parent_id, leaf.label, nodes)
-    id_count.increase()
-
-    identifier_leaf = id_count.identifier
-    add_node(identifier_leaf, identifier_node, leaf.text, nodes)
-    id_count.increase()
-
-
-def add_node(identifier: int, parent_id: int, label: str, nodes: Nodes):
+def make_node(identifier: int, parent_id: int, label: str, prob: float = None):
     """
     Adds a node and its corresponding information to the nodes list, in json/dictionary format
     :param identifier: The id given to the node/leaf currently being added, giving this node the currently highest index
     :param parent_id: The id of the parent of the node/leaf currently being added
     :param label: The name of the node/leaf currently being added
-    :param nodes: The class containing the list of nodes, destination of this node
+    :param prob: certainty of the label on the leaf
     """
-    template = "<div class=\"domStyle\"><span>" + label + "</span></div>"
+
+    if prob:
+        cert = round(prob * 100)
+        template = "<div class=\"domStyle\"><span>" + label + "</span></div><span class=\"confidence\">" \
+                   + str(cert) + "%</span>"
+    else:
+        template = "<div class=\"domStyle\"><span>" + label + "</span></div>"
 
     node = {"nodeId": str(identifier),
             "parentNodeId": str(parent_id),
+            "width": 347,
+            "height": 147,
+            "borderRadius": 15,
             "template": template,
             "alternatives": None,
             "label": str(label)}
-    nodes.add_node(node)
+
+    return node
 
 
-def send_to_front(nodez: Nodes):
+def send_to_front(nodes: list):
     """
     Sends the desired json structure to the UI
     """
     # TODO: This is a temporary
     with open('Place your file here folks', 'w') as outfile:
-        json.dump(nodez.nodes, outfile)
+        json.dump(list, outfile)
+
+    return list
 
 
 if __name__ == "__main__":
     test_tree = get_tree("cheese")
-    test_nodes = Nodes()
-    test_id_counter = IdCounter()
-    traverse(test_tree, test_id_counter, test_nodes)
-    print(test_nodes.nodes)
-    pprint.PrettyPrinter().pprint(test_nodes.nodes)
+    json_file = get_tree2(test_tree)
+    print(json_file)
+    pprint.PrettyPrinter().pprint(json_file)
