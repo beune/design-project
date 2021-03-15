c0a030f1a857d685e3b78328eeb9871330bec634
Pim Beune <git@beune.dev>
2021-03-15 10:55:59 +0100

    Fix flake8 errors

--- src/client/app.py
+++ src/client/app.py
@@ -2,15 +2,18 @@
 
 import eel
 
-eel.init('web') # or the name of your directory
+eel.init('web')  # or the name of your directory
+
 
 @eel.expose
 def test(text):
     print(text)
 
+
 @eel.expose
 def hello_world():
     print("Hello from python")
+
 
 eel.start('index.html', size=(600, 400))
 # eel.start()
