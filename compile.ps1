8e2edec89539a4bc169f53d96fde5673b8af5ffc
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-03-17 18:56:11 +0100

    changed target in compile.ps1 and removed app.py controller.py will be compiled instead of app.py

--- compile.ps1
+++ compile.ps1
@@ -3,5 +3,5 @@ Set-Location -Path src\client\frontend
 npm install
 npm run build
 Set-Location -Path ..
-python -m eel app.py web --onefile
+python -m eel controller.py web --onefile
 Write-Output "Done"
