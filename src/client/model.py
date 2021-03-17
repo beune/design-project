fc9d7e3f57c3e80eaa181e91704a92331491cd5d
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-03-17 18:50:56 +0100

    wip: Setup for the eel-front_end communication

--- src/client/model.py
+++ src/client/model.py
@@ -1,7 +1,48 @@
 """
 Imports
 """
-# Slechts de naam van de environment, de environments zelf zitten alleen op de server
-environment: str
-changes = []
-text: str
+from typing import Callable
+
+import jsonpickle
+import requests
+
+from src.report_node import ReportNode
+
+ENDPOINT = "http://127.0.0.1:5000/"
+
+
+class Model:
+    """
+    Model contains the state of the client application.
+    """
+    def __init__(self, notify: Callable[['Model'], None]):
+        self.environment = "mammo"
+        self.text = ""
+        self.tree = ReportNode("Root")
+        self.notify_view = notify
+
+    def retrieve_tree(self):
+        """
+        Use
+        """
+        data = {"text": self.text}
+        response = requests.get(ENDPOINT + self.environment, json=data)
+        self.tree = jsonpickle.decode(response.json()["Data"])
+
+    def set_text(self, new_text: str):
+        """
+        Store the new_text, update the tree and notify the view.
+        :param new_text: the new text.
+        """
+        self.text = new_text
+        self.retrieve_tree()
+        self.notify_view(self)
+
+    def set_environment(self, new_environment: str):
+        """
+        Set the environment to new_environment, update the tree and notify the view.
+        :param new_environment: the new environment.
+        """
+        self.environment = new_environment
+        self.retrieve_tree()
+        self.notify_view(self)
