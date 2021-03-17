780cc719bb0b8b9bc26d332f3457f13ad396698e
Jorrit van Assen <j.s.vanassen@student.utwente.nl>
2021-03-17 18:50:56 +0100

    wip: Setup for the eel-front_end communication

--- src/client/app.py
+++ src/client/app.py
@@ -15,5 +15,9 @@ def hello_world():
     print("Hello from python")
 
 
-eel.start('index.html', size=(600, 400))
-# eel.start()
+eel.start('index.html', size=(600, 400), block=False)
+# eel.start('index.html', size=)
+while True:
+    print("I'm a main loop")
+    eel.set_tree_data("tree")
+    eel.sleep(1.0)
