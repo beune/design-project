1c974881f22d221e482d0ddda199b7bfa2ea8ffa
Stan Ritsema <s.ritsema@student.utwente.nl>
2021-03-18 13:52:09 +0100

    Added the entire test text in the client

--- src/client/client.py
+++ src/client/client.py
@@ -9,6 +9,7 @@ def connect():
     """
     Method used to test API calls
     """
+    # text = "X-mammografie beiderzijds: Een stervormige laesie laterale bovenkwadrant linkermamma, De laesie heeft een body van ongeveer 2,3 cm, De laesie is wel moeilijk af te grenzen van het normale klierweefsel,  In en rondom de laesie geen microcalcificaties, In de rechtermamma voor zover te beoordelen geen aanwijzingen voor maligniteit, Axillair beiderzijds geen pathologische lymfeklieren zichtbaar, "
     text = "X-mammografie beiderzijds: Een stervormige laesie laterale bovenkwadrant linkermamma"
     data = {"text": text}
     # r = requests.get("http://127.0.0.1:5000/mammo/", params=data)
