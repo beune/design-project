c4e2bfc1c3fcb9b1c824b8b7f1be565f9cbb5d06
Pim Beune <git@beune.dev>
2021-03-23 11:46:28 +0100

    Remove title

--- client/frontend/src/App.vue
+++ client/frontend/src/App.vue
@@ -90,16 +90,6 @@
 
       <!-- Sizes your content based upon application components -->
       <v-main>
-        <v-app-bar
-          :elevation="0"
-          dense
-        >
-          <v-list-item>
-            <v-list-item-content>
-              Mammografie 22-09-16
-            </v-list-item-content>
-          </v-list-item>
-        </v-app-bar>
         <!-- Provides the application the proper gutter -->
         <v-container fluid>
           <d3orgtree
