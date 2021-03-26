0e093de1ffb3cfd9a90c8d034f667ade0df1a404
bas_k <bas_kool@live.nl>
2021-03-26 16:13:33 +0100

    added setup for tests

--- test/test_client/test_view.py
+++ test/test_client/test_view.py
@@ -10,18 +10,27 @@ from client import model
 from report_tree.report_node import ReportNode
 from report_tree.report_leaf import ReportLeaf
 
+
 class ViewTest(unittest.TestCase):
-    def __init__(self):
-        report_leaf = ReportLeaf("stervormige", "margin", 0.55, )
-        self.input = None
 
     def test_build_json_tree(self):
-        json_tree = view.generate_tree(self.input)
+        report_leaf_a = ReportLeaf('stervormige', 'margin', 0.55,
+                                   {'stervormige': 55, 'circumscribed': 0, 'obscured': 0})
+        report_node_1 = ReportNode('mass', [report_leaf_a], ['shape', 'margin', 'density'])
+        report_node_2 = ReportNode('positive_finding', [report_node_1], [])
+        root = ReportNode('root', [report_node_2], [])
+        json_tree = view.generate_tree(root)
+        self.assertEqual(json_tree[0]["nodeId"], "root")
+        self.assertEqual(json_tree[0]["parentNodeId"], None)
+
+
+
+
+
+
     #     self.assertEqual(json_tree)
     #
     # def test_hash_function(self):
     #
     #
     # def test_apply_changes(self):
-
-
