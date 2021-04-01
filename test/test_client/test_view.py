1b3aba2f6382e22538283e40a25273b0b137ae2e
TomH 123 <t.h.postmus@student.utwente.nl>
2021-04-01 12:06:31 +0200

    Changes by bas_k

--- test/test_client/test_view.py
+++ test/test_client/test_view.py
@@ -84,54 +84,6 @@ class ViewTest(unittest.TestCase):
         self.assertEqual(json_tree[7]["speculative"], True)
         self.assertEqual(json_tree[7]["label"], spec_c_field)
 
-    def test_identifier_function(self):
-        node_1_label = 'mass'
-        node_2_label = 'positive_finding'
-        node_3_label = 'negative_finding'
-        root_label = 'root'
-
-        report_node_1 = ReportNode(node_1_label, [])
-        report_node_2 = ReportNode(node_2_label, [report_node_1])
-        report_node_3 = ReportNode(node_1_label, [])
-        report_node_4 = ReportNode(node_2_label, [report_node_3])
-        report_node_5 = ReportNode(node_1_label, [])
-        report_node_6 = ReportNode(node_3_label, [report_node_5])
-        report_node_7 = ReportNode(node_1_label, [])
-        report_node_8 = ReportNode(node_3_label, [report_node_7])
-        root = ReportNode(root_label, [report_node_2, report_node_4, report_node_6, report_node_8])
-        json_tree = view.generate_tree(root, {})
-
-        # pos_fin
-        self.assertEqual(json_tree[1]["nodeId"], "_".join([node_2_label, root_label]))
-        self.assertEqual(json_tree[1]["parentNodeId"], root_label)
-
-        # mass_pos_fin
-        self.assertEqual(json_tree[2]["nodeId"], "_".join([node_1_label, node_2_label, root_label]))
-        self.assertEqual(json_tree[2]["parentNodeId"], "_".join([node_2_label, root_label]))
-
-        # pos_fin duplicate
-        self.assertEqual(json_tree[3]["nodeId"], "_".join([node_2_label, root_label]) + "2")
-        self.assertEqual(json_tree[3]["parentNodeId"], root_label)
-
-        # mass_pos_fin duplicate
-        self.assertEqual(json_tree[4]["nodeId"], "_".join([node_1_label, node_2_label, root_label]) + "2")
-        self.assertEqual(json_tree[4]["parentNodeId"], "_".join([node_2_label, root_label]) + "2")
-
-        # neg_fin
-        self.assertEqual(json_tree[5]["nodeId"], "_".join([node_3_label, root_label]))
-        self.assertEqual(json_tree[5]["parentNodeId"], root_label)
-
-        # mass_neg_fin
-        self.assertEqual(json_tree[6]["nodeId"], "_".join([node_1_label, node_3_label, root_label]))
-        self.assertEqual(json_tree[6]["parentNodeId"], "_".join([node_3_label, root_label]))
-
-        # neg_fin duplicate
-        self.assertEqual(json_tree[7]["nodeId"], "_".join([node_3_label, root_label]) + "2")
-        self.assertEqual(json_tree[7]["parentNodeId"], root_label)
-
-        # mass_neg_fin duplicate
-        self.assertEqual(json_tree[8]["nodeId"], "_".join([node_1_label, node_3_label, root_label]) + "2")
-        self.assertEqual(json_tree[8]["parentNodeId"], "_".join([node_3_label, root_label]) + "2")
 
     def test_apply_changes(self):
         leaf_a_label = 'stervormige'
